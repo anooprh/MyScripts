4061e39ad4e6ae4e7df6ee8334857bef72aa7e6b
Anoop <anooprh@thoughtworks.com>
2013-06-30 21:10:52 +0530

    Xmonad Key Config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,8 +3,29 @@
 -- Anoop
 
 import XMonad
+import XMonad.Layout.WindowNavigation
+
+import qualified Data.Map as M
 
 main = xmonad $ defaultConfig
-    { borderWidth        = 5
-    , modMask = mod4Mask
-    , normalBorderColor  = "#cccccc" }
+    { borderWidth       = 5
+    , modMask           = mod4Mask     --Super / Windows :( Key
+    , normalBorderColor = "#FFFFFF"   
+    , focusedBorderColor= "#FF0000"    
+    , focusFollowsMouse = True
+    --, keys              = myKeyConfig  --Custom Key Config defined below
+    }
+
+-- My Custom Key Configurations
+customKeyConfig conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask, xK_Right), sendMessage $ Go R)
+    , ((modMask, xK_Left), sendMessage $ Go L)
+    , ((modMask, xK_Up), sendMessage $ Go U)
+    , ((modMask, xK_Down), sendMessage $ Go D)
+    , ((modMask, xK_l), sendMessage $ Go R)
+    , ((modMask, xK_h), sendMessage $ Go L)
+    , ((modMask, xK_k), sendMessage $ Go U)
+    , ((modMask, xK_j), sendMessage $ Go D)
+    ]
+--Union of custom Key Config with default key config
+myKeys x = customKeyConfig x `M.union` keys defaultConfig x
